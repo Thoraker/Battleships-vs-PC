09a0d62426441a48b3e6e2f8075c1cfa59cc9c43
Félix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2023-10-23 03:56:24 +0000

    Adding React-Router

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -7,7 +7,8 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { useTheme } from "@/components/theme-provider"
+import { useTheme } from "@/hooks/theme-provider-hook"
+
 
 export function ModeToggle() {
   const { setTheme } = useTheme()
